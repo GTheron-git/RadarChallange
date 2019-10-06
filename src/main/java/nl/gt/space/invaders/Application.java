28d530bb8818d6729e4f89d91e75162603186caf
GTheron-git <56213535+GTheron-git@users.noreply.github.com>
2019-10-06 07:52:58 +0200

    Add files via upload

---
+++ src/main/java/nl/gt/space/invaders/Application.java
@@ -0,0 +1,13 @@
+package nl.gt.space.invaders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
